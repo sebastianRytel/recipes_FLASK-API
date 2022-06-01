0654e8ccab071fa2602fd2d2fa608b31b8869981
sebastianRytel <sebastian.rytel@gmail.com>
2022-06-01 16:40:09 +0200

    Final app.

--- app.py
+++ app.py
@@ -1,96 +1,145 @@
+import json
+import sys
+import re
+from sqlalchemy import any_
+import tests
+
 from flask import Flask, request, jsonify, make_response
+from werkzeug.exceptions import abort
+
 from db import db
 from models.recipe_has_product import IngredientModel
 from models.recipe_has_product import RecipeModel
-from models.recipe_has_product import RecipeHasProductModel
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///recipes.db'
 
-
 db.init_app(app)
 
 with app.app_context():
+    db.drop_all()
     db.create_all()
+
+def input_validation(response):
+    for key in ["title", "directions", "description", "ingredients"]:
+        if key not in response.keys():
+            return False
+    for k, v in response.items():
+        if not v:
+            return False
+    return True
 
 
 @app.route("/api/recipe/new", methods=["POST"])
 def recipe():
-
     response = request.get_json()
-    # # try:
-    # #     response = json.loads(response)
-    # # except json.JSONDecodeError:
-    # #     return make_response("Invalid input", 400)
-    # for key in ["title", "directions", "ingredients"]:
-    #     if key not in response.keys():
-    #         return make_response("Invalid input", 400)
-    #
-    #
-    #
+    try:
+        response = json.loads(response)
+    except json.JSONDecodeError:
+        return make_response("Invalid input", 400)
+
+    if not input_validation(response):
+        return make_response("Invalid input", 400)
+
     new_recipe = {
         "title": response["title"],
         "directions": response["directions"],
+        "description": response["description"],
         "ingredients": response["ingredients"],
     }
 
-    recip = RecipeModel(title=new_recipe["title"], directions=new_recipe["directions"])
-    recip.save_to_db()
-    for ingredient in new_recipe["ingredients"]:
-        new_ingredient = IngredientModel(title=ingredient["title"], measure=ingredient["measure"], amount=ingredient["amount"])
-        new_ingredient.save_to_db()
-
-    return jsonify({"id": recip.id}), 200
-
-    # recipes.append(new_recipe)
-    # return jsonify({"id": new_recipe["id"]}), 200
-
-
-# @app.route("/api/recipe", methods=["GET"])
-# def get_recipe():
-#     if not recipes:
-#         return jsonify({{"error": "No recipe here yet"}})
-#     ingredients = request.args
-#     split_ingredients = ingredients.get("ingredients").split("|")
-#     matching_recips = []
-#     not_matching_recips = []
-#     for recip in recipes:
-#         ingredients = recip.get("ingredients")
-#         ingr = [ingredient.get("title") for ingredient in ingredients]
-#         remaining_ingredients = set(ingr) - set(split_ingredients)
-#
-#         if not remaining_ingredients:
-#             matching_recips.append(recip)
-#
-#         elif remaining_ingredients:
-#             not_matching_recips.append(recip)
-#
-#     if matching_recips:
-#         return jsonify(matching_recips), 200
-#     elif not matching_recips:
-#         return jsonify({"error": "No recipe here yet"})
-#     elif not_matching_recips:
-#         return jsonify({"error": "No recipe for these ingredients"})
-#
-#
-#
-# @app.route("/api/recipe/<int:id>", methods=["GET"])
-# def get_recipe_by_id(id):
-#     for recip in recipes:
-#         if recip["id"] == id:
-#             print(recip["id"])
-#             return {
-#                 "title": recip["title"],
-#                 "directions": recip["directions"],
-#                 "ingredients": recip["ingredients"],
-#             }
-#     return jsonify("Recip not found"), 404
-#
-#
-# @app.route("/api/recipe/all", methods=["GET"])
-# def get_recipe_all():
-#     return jsonify({"recipes": recipes})
+    recipe = RecipeModel.query.filter_by(title=new_recipe['title']).first()
+    if not recipe:
+        recip = RecipeModel(title=new_recipe["title"], description=new_recipe["description"])
+        recip.directions = response['directions']
+        recip.save_to_db()
+        for ingredients in new_recipe["ingredients"]:
+            new = IngredientModel(**ingredients)
+            recip.ingredients.append(new)
+            new.save_to_db()
+        return jsonify({"id": recip.id}), 200
+    else:
+        return jsonify({'message': f'Recipe "{new_recipe["title"]}" already exists'})
 
 
-if __name__ == "__main__":
-    app.run(debug=True)
+@app.route("/api/recipe", methods=["GET"])
+def get_recipe():
+    all_recipes = RecipeModel.query.all()
+    if not all_recipes:
+        return jsonify({"error": "No recipe here yet"})
+    ingredients = request.args
+    max_directions = ingredients.get("max_directions")
+    split_ingredients = ingredients.get("ingredients").split("|")
+    matching_recips = []
+    for recip in all_recipes:
+        ingredients = recip.get_ingredients()
+        remaining_ingredients = set(ingredients) - set(split_ingredients)
+        if not remaining_ingredients:
+            matching_recips.append(recip.json_get_with_id())
+    if matching_recips:
+        for x in matching_recips:
+            if max_directions:
+                if len(x['directions']) > int(max_directions):
+                    return jsonify([])
+        return jsonify(sorted(matching_recips, key=lambda x: x['title'], reverse=True)), 200
+    return jsonify(matching_recips)
+
+
+@app.route("/api/recipe/<int:id>", methods=["GET"])
+def get_recipe_by_id(id):
+    recipe = RecipeModel.query.filter_by(id=id).first()
+    if not recipe:
+        return make_response("Recip not found"), 404
+    return recipe.json_get_without_id()
+
+
+@app.route("/api/recipe/<int:id>", methods=["PUT"])
+def update_recipe_by_id(id):
+    recipe_by_id = RecipeModel.query.filter_by(id=id).first()
+    if not recipe_by_id:
+        return make_response("Recip not found"), 404
+
+    response = request.get_json()
+    response = json.loads(response)
+
+    if not input_validation(response):
+        return make_response("Invalid input", 400)
+
+    if recipe_by_id:
+        db.session.delete(recipe_by_id)
+        db.session.commit()
+
+        new_recipe = {
+            "title": response["title"],
+            "directions": response["directions"],
+            "description": response["description"],
+            "ingredients": response["ingredients"],
+        }
+
+        recip = RecipeModel(title=new_recipe["title"], description=new_recipe["description"])
+        recip.directions = response['directions']
+        recip.save_to_db()
+        for ingredients in new_recipe["ingredients"]:
+            new = IngredientModel(**ingredients)
+            recip.ingredients.append(new)
+            new.save_to_db()
+
+    return make_response(""), 204
+
+
+@app.route("/api/recipe/<int:id>", methods=["DELETE"])
+def delete_recipe_by_id(id):
+    recipe = RecipeModel.query.filter_by(id=id).first()
+    if recipe:
+        db.session.delete(recipe)
+        db.session.commit()
+        return make_response("", 204)
+    return abort(404)
+
+
+if __name__ == '__main__':
+    if len(sys.argv) > 1:
+        arg_host, arg_port = sys.argv[1].split(':')
+        app.run(host=arg_host, port=arg_port)
+    else:
+        app.run(debug=True)
--- models/recipe_has_product.py
+++ models/recipe_has_product.py
@@ -1,12 +1,10 @@
+from sqlalchemy.ext.hybrid import hybrid_property
+
 from db import db
 
-
-class RecipeHasProductModel(db.Model):
-    __tablename__ = "recipe_has_product"
-
-    id = db.Column(db.Integer, primary_key=True)
-    id_ingredient = db.Column(db.Integer, db.ForeignKey("ingredient.id"))
-    id_recipe = db.Column(db.Integer, db.ForeignKey("recipe.id"))
+recipe_has_product = db.Table("recipe_has_product", db.Model.metadata,
+                              db.Column('ingredient_id ', db.Integer, db.ForeignKey("ingredients.id")),
+                              db.Column("recipe_id", db.Integer, db.ForeignKey("recipe.id")))
 
 
 class RecipeModel(db.Model):
@@ -14,26 +12,62 @@ class RecipeModel(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(80))
-    directions = db.Column(db.String(80))
-    recipe_has_product = db.relationship("RecipeHasProductModel")
+    description = db.Column(db.String(80))
+    _directions = db.Column(db.String(500))
 
-    def __init__(self, title: str, directions: str):
+    ingredients = db.relationship("IngredientModel", secondary=recipe_has_product, backref=db.backref("ingredients", lazy='joined'), cascade="all, delete")
+
+    def __init__(self, title: str, description: str):
         self.title = title
-        self.directions = directions
+        self.description = description
 
     def save_to_db(self):
         db.session.add(self)
         db.session.commit()
 
+    @property
+    def directions(self):
+        return [direction for direction in self._directions.split("|")]
+
+    @directions.setter
+    def directions(self, list_of_directions):
+        self._directions = "|".join(direction for direction in list_of_directions)
+
+
+    def json_get_with_id(self):
+        return {
+            "id": self.id,
+            "title": self.title,
+            "description": self.description,
+            "directions": self.directions,
+            "ingredients": [ingredient.json() for ingredient in self.ingredients]
+        }
+
+    def json_get_without_id(self):
+        return {
+            "title": self.title,
+            "description": self.description,
+            "directions": self.directions,
+            "ingredients": [ingredient.json() for ingredient in self.ingredients]
+        }
+
+    def get_short_info(self):
+        return {
+            "id": self.id,
+            "title": self.title,
+            "ingredients": [ingredient.json() for ingredient in self.ingredients]
+        }
+
+    def get_ingredients(self):
+        return [ingredient.json()['title'] for ingredient in self.ingredients]
 
 class IngredientModel(db.Model):
-    __tablename__ = "ingredient"
+    __tablename__ = "ingredients"
 
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(80))
     measure = db.Column(db.String(10))
     amount = db.Column(db.Float)
-    recipe_has_product = db.relationship("RecipeHasProductModel")
 
     def __init__(self, title: str, measure: float, amount: int):
         self.title = title
@@ -43,3 +77,11 @@ class IngredientModel(db.Model):
     def save_to_db(self):
         db.session.add(self)
         db.session.commit()
+
+
+    def json(self):
+        return {
+            "title": self.title,
+            "measure": self.measure,
+            "amount": self.amount,
+        }
